d22f97865ec486cb16107f05793ec800d37c1ab3
Evgeny Antipov <e.antipov@gmail.com>
2022-09-11 19:36:57 +0300

    new branch .bin

--- src/Basket.java
+++ src/Basket.java
@@ -1,12 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.io.*;
+
 
 public class Basket {
 
@@ -35,27 +28,20 @@ public class Basket {
         System.out.println("Итого " + sumProduct + " руб.");
     }
 
-    public void saveTxt(File textFile) throws FileNotFoundException {
-        PrintWriter out = new PrintWriter(textFile);
-        Stream.of(product).forEach(i ->
-                out.printf(i.getName(), i.getPrice(), i.getInBasket()));
-        out.close();
+    public void saveBin(File file){
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public static Basket loadFromTxtFile(File textFile) throws FileNotFoundException, ParseException {
-        Scanner scanner = new Scanner(textFile);
-        List<Product> product = new ArrayList<>();
-        String name;
-        int price;
-        int inBasket;
-        NumberFormat nf = NumberFormat.getInstance();
-        while (scanner.hasNext()) {
-            String[] parts = scanner.nextLine().split("@");
-            name = parts[0];
-            price = nf.parse(parts[1]).intValue();
-            inBasket = Integer.parseInt(parts[2]);
-            product.add(new Product(name, price, inBasket));
-        }
-        return new Basket(product.toArray(Product[]::new));
+    public static Basket loadFromBinFile(File file) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        return (Basket) ois.readObject();
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Scanner;
 
@@ -11,59 +12,53 @@ public class Main {
             new Product("Молоко", 300)
     };
 
-    public static void main(String[] args) throws FileNotFoundException, ParseException {
-        Basket card;
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Basket card = new Basket(product);
         int productNumber;
         int productCount;
 
         Scanner scanner = new Scanner(System.in);
 
-        File basketFile = new File("basket.txt");
+        File basketFile = new File("basket.bin");
         if (basketFile.exists()) {
             System.out.println("Загрузить корзину(нажмите enter)? ");
             if (scanner.nextLine().equals("")) {
-                card = Basket.loadFromTxtFile(basketFile);
-            } else {
-                card = new Basket(product);
-            }
-        } else {
-            card = new Basket(product);
-        }
-
-        while (true) {
-            System.out.println("Список доступных товаров для покупки");
-
-            for (int i = 0; i < product.length; i++) {
-                System.out.println((i + 1) + ". " + product[i].getName() + " " + product[i].getPrice() + " pуб/шт");
+                card = Basket.loadFromBinFile(basketFile);
             }
 
-            System.out.println("Выберите товар и количество или введите `end`");
-            String inputString = scanner.nextLine(); // "1 10"
-            String[] parts = inputString.split(" ");
-            if (parts.length == 2) {
-                try {
-                    productNumber = Integer.parseInt(parts[0]);
-                    productCount = Integer.parseInt(parts[1]);
-                    if (productNumber <= 0 || productNumber > product.length) {
-                        System.out.print("Неверный номер товара");
-                        continue;
-                    }
-                    if (productCount <= 0) {
-                        continue;
-                    }
-                    card.addToCart(productNumber - 1, productCount);
-                    card.saveTxt(basketFile);
-                } catch (NumberFormatException nfe) {
-                    System.out.println("Необходимо указать два целых числа");
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException(e);
+            while (true) {
+                System.out.println("Список доступных товаров для покупки");
+
+                for (int i = 0; i < product.length; i++) {
+                    System.out.println((i + 1) + ". " + product[i].getName() + " " + product[i].getPrice() + " pуб/шт");
                 }
-            } else if (inputString.equals("end")) {
-                break;
+
+                System.out.println("Выберите товар и количество или введите `end`");
+                String inputString = scanner.nextLine(); // "1 10"
+                String[] parts = inputString.split(" ");
+                if (parts.length == 2) {
+                    try {
+                        productNumber = Integer.parseInt(parts[0]);
+                        productCount = Integer.parseInt(parts[1]);
+                        if (productNumber <= 0 || productNumber > product.length) {
+                            System.out.print("Неверный номер товара");
+                            continue;
+                        }
+                        if (productCount <= 0) {
+                            continue;
+                        }
+                        card.addToCart(productNumber - 1, productCount);
+                        card.saveBin(basketFile);
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Необходимо указать два целых числа");
+                    }
+                } else if (inputString.equals("end")) {
+                    break;
+                }
+                System.out.println("Укажите следующую пару чисел");
             }
-            System.out.println("Укажите следующую пару чисел");
+            scanner.close();
+            card.printCart();
         }
-        scanner.close();
-        card.printCart();
     }
 }
